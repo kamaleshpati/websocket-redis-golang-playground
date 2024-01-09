6c8b36b2e3bfb6c289d3b4513ab95f3547c048d3
fivetran-kamaleshpati <83626034+fivetran-kamaleshpati@users.noreply.github.com>
2024-01-09 13:13:13 +0530

    sigleton db instance created

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,12 +1,33 @@
 package database
 
 import (
+	"sync"
+
 	"github.com/go-redis/redis"
 
 	"github.com/kamaleshpati/wsredisPlayground/internal/utils"
 )
 
+var clientInstance *redis.Client
+var lock = &sync.Mutex{}
+
 func GetRedisClient() *redis.Client {
+	return getSingleInstance(clientInstance)
+}
+
+func getSingleInstance(singleInstance *redis.Client) *redis.Client {
+	if singleInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singleInstance == nil {
+			singleInstance = createInstance()
+		}
+	}
+
+	return singleInstance
+}
+
+func createInstance() *redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr:     utils.GetEnvironmentVariable("DBHOST"),
 		Password: "", // no password set
